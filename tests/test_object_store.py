574173005a022de58c8d070800db2acb329d26d7
Dean Serenevy <dean@serenevy.net>
2020-05-10 18:42:56 -0400

    test_object_store: Normalize file, remove six, quell warnings

--- tests/test_object_store.py
+++ tests/test_object_store.py
@@ -1,27 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: LGPL-3.0
 import unittest
-
-import six
 
 import amethyst.games
 from amethyst.games.filters import Filter, Filterable
 from amethyst.games.plugins import ObjectStore
 
 
-class TestEngine(amethyst.games.Engine):
+class Engine(amethyst.games.Engine):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.register_plugin(TestGame())
         self.register_plugin(ObjectStore())
         self.players.append(0)
 
 
-class TestGame(amethyst.games.EnginePlugin):
-    AMETHYST_PLUGIN_COMPAT = 1  # Plugin API version
-
-
 class ObjectStoreListShared(unittest.TestCase):
     def setUp(self):
-        self.game = TestEngine()
+        self.game = Engine()
 
     def test_empty(self):
         self.assertEqual(self.game.stor_list_shared(), [])
@@ -34,7 +30,7 @@ class ObjectStoreListShared(unittest.TestCase):
         ]
         self.game.stor_extend_shared(objects)
 
-        six.assertCountEqual(self, self.game.stor_list_shared(), objects)
+        self.assertCountEqual(self.game.stor_list_shared(), objects)
 
     def test_with_filter(self):
         objects = [
@@ -44,8 +40,7 @@ class ObjectStoreListShared(unittest.TestCase):
         ]
         self.game.stor_extend_shared(objects)
 
-        six.assertCountEqual(
-            self,
+        self.assertCountEqual(
             self.game.stor_list_shared(Filter(flag='a')),
             [objects[0], objects[2]]
         )
